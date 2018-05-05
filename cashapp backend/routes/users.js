a2ba9cf103f7a42cdc7fcfd9a800f9d3aadfd29a
sunny-udhani <sunny.iv09@gmail.com>
2018-05-04 23:08:30 -0700

    updates
    updates

--- cashapp backend/routes/users.js
+++ cashapp backend/routes/users.js
@@ -3,312 +3,146 @@ var router = express.Router();
 var mysql = require('mysql');
 var bcrypt = require('bcrypt');
 const saltRounds = 5;
-var axios=require('axios');
+var axios = require('axios');
 //const redis = require('redis');
 //var cors = require('cors');
 var mongoose = require('mongoose');
 //require('./passport')(passport);
 //var passport=require('passport');
 var passport = require('passport')
-  , LocalStrategy = require('passport-local').Strategy;
+    , LocalStrategy = require('passport-local').Strategy;
 
-var users=require('../models/users.js');
-var user_details=require('../models/userdetails.js');
+var users = require('../models/users.js');
+var user_details = require('../models/userdetails.js');
 var mongoSessionURL = 'mongodb://cashapp:cashapp@ds215380.mlab.com:15380/cashapp';
-var db='mongodb://cashapp:cashapp@ds215380.mlab.com:15380/cashapp';
-mongoose.Promise=global.Promise;
-mongoose.connect(db,function(err)
-{
-  if(err)
-  {
-    console.log("Error" +err);
-  }
-  else{
-    console.log("Connected to Mongodb");
-  }
+var db = 'mongodb://cashapp:cashapp@ds215380.mlab.com:15380/cashapp';
+mongoose.Promise = global.Promise;
+mongoose.connect(db, function (err) {
+    if (err) {
+        console.log("Error" + err);
+    }
+    else {
+        console.log("Connected to Mongodb");
+    }
 })
 
-//get method to get all user details
-router.get('/userdetails',function(req,res)
-{
-  if(req.session.email)
- {
-   console.log(req.session.email);
-  console.log('User details request API called');
-  user_details.findOne({'email_address':req.session.email})
-  .exec(function(err,users)
-{
-  if(err)
-  {
-    console.log("error in finding data from the users");
-  }
-  console.log(users);
-    res.json(users);
-})
+//get method to get all user details 
+router.get('/userdetails', function (req, res) {
+        if (req.session.email) {
+            console.log(req.session.email);
+            console.log('User details request API called');
+            user_details.findOne({'email_address': req.session.email})
+                .exec(function (err, users) {
+                    if (err) {
+                        console.log("error in finding data from the users");
+                    }
+                    console.log(users);
+                    res.json(users);
+                })
 
-  }
-  else{
-    res.json("Session not implemented");
-  }
-}
+        }
+        else {
+            res.json("Session not implemented");
+        }
+    }
 )
 
 
 //signup API
-router.post('/signup', function(req,res)
-{
-  console.log("Signup API called");
-  var newusers=new users;
+router.post('/signup', function (req, res) {
+    console.log("Signup API called");
+    var newusers = new users;
     newusers.email_address = req.body.email_address;
-    newusers.username= req.body.username;
-    newusers.password= req.body.password;
-    newusers.debitcard=req.body.debitcard;
+    newusers.username = req.body.username;
+    newusers.password = req.body.password;
+    newusers.debitcard = req.body.debitcard;
 
 
+    console.log("Parameters taken successfully");
+    let apiPayload = {
+        Key: req.body.email_address,
+        Value: {
+            password: req.body.password,
+            username: req.body.username,
+            debitcard: req.body.debitcard,
+            balance: 0
 
-  console.log("Parameters taken successfully");
-  let apiPayload={
-    Key:req.body.email_address,
-    Value:{
-      password:req.body.password,
-      username:req.body.username,
-      debitcard:req.body.debitcard
-
-  }
-}
+        }
+    }
 //console.log(apiPayload.ID);
 //console.log(apiPayload.Value);
-console.log(apiPayload);
-let GoUrl="http://localhost:3010/redis_set"
+    console.log(apiPayload);
+    let GoUrl = "http://52.53.126.123:3000/redis_set"
 
-axios.post(GoUrl,apiPayload)
-.catch((err)=>{
-  res.json("Signup Unsuccessful");
+    axios.post(GoUrl, apiPayload)
+        .then((result) => {
+            console.log("Go API hit");
+            console.log(result);
+
+            res.json("Signup Successful");
+        })
+        .catch(err => {
+            console.log(err);
+            res.json("Signup Unsuccessful");
+
+        })
 })
-.then((result)=>{
-    console.log("Go API hit");
-    res.json("Signup Successful");
-})
-})
-
-  //var length=newusers.password.length;
-  //newusers.save(function(err,user)
-//{
-
-  //if(err)
- // {
-   // console.log(err);
-    //console.log("Error received from ")
-    //result="Signup unsuccessful";
-    //res.json(result);
- // }
- // else{
-   // console.log("Received from createUser method");
-    // var result="Signup Successful";
-    //res.json(result);
-
-  //}
-//})
-
-  //newusers.save(function(err, inserteduser)
-//{
- // if(err)
- // {
-  //  console.log("Data not inserted into users since user already exists");
-  //   res.json("Signup unsuccessful");
-  //}
-  //else if (newusers.password ==="" ||newusers.email_address==="" || newusers.username==="")
-  //{
-  //   res.json("Missing some fill outs");
-  //}
-  //else if(length<6)
-  //{
-  //  res.json("Password too short")
- // }
-  //else{
-
-  //  res.json("Signup Successful");
- // }
-//})
-
 
 //login API
-router.post('/login', function(req,res)
-{
-  console.log("Login API called");
-  var newusers1=new users;
-  newusers1.email_address = req.body.email_address;
+router.post('/login', function (req, res) {
+        console.log("Login API called");
+        var newusers1 = new users;
+        newusers1.email_address = req.body.email_address;
 
-  newusers1.password= req.body.password;
-  console.log(req.body.email_address);
-  console.log(req.body.password);
+        newusers1.password = req.body.password;
+        console.log(req.body.email_address);
+        console.log(req.body.password);
 
-  if(!req.body.email_address||!req.body.password)
-  {
-    console.log("Please fill out fields");
-    res.json("Please fill out fields");
-  }
-else{
-  users.findOne({'email_address':req.body.email_address})
-  .exec(function(err,users)
-{
-  if(err)
-  {
-    console.log("error in finding data from the users");
-    res.json("Please enter correct credentials");
-  }
-  else if(!users)
-  {
-    res.json("Please enter correct credentials");
-  }
+        if (!req.body.email_address || !req.body.password) {
+            console.log("Please fill out fields");
+            res.json("Please fill out fields");
+        }
+        else {
 
-  else if(users.password===req.body.password)
-  {
-    console.log(users);
-    console.log("Login Successful")
-    req.session.email=newusers1.email_address;
-    res.json("Login Successful");
+            let apiPayload = {
+                Key: req.body.email_address
+            };
 
-    console.log(req.session.email);
-  }
-  else{
-    res.json("Please enter correct credentials");
-  }
+            console.log(apiPayload);
+            let GoUrl = "http://52.53.126.123:3000/redis_get";
 
-})
-}
+            axios.post(GoUrl, apiPayload)
+                .then((result) => {
+                    console.log("Go API hit");
+                    console.log(result);
 
-  //console.log(email);
-  //console.log(password1);
+                    if (result.data.Value.password === req.body.password) {
+                        req.session.email = req.body.email_address;
+                        res.json("Login Successful");
+                    } else {
+                        res.json("Please enter correct credentials");
+                    }
 
+                })
+                .catch(err => {
+                    console.log(err);
+                    res.json("Login Unsuccessful");
 
-}
-);
+                })
+        }
 
-passport.serializeUser(function(users, done) {
-  done(null, users.email_address);
-});
+        //console.log(email);
+        //console.log(password1);
 
-passport.deserializeUser(function(id, done) {
-  users.getUserById(id, function(err, users) {
-    done(err, users);
-  });
-});
-passport.use( new LocalStrategy(
-{
-usernameField:'email_address',
-passwordField:'password'
-
-},
-  function(username, password, done) {
-  console.log('in passport');
-  console.log(username);
-  console.log(password);
-  var query={'email_address':username}
-
-  users.findOne(query)
-  .exec(function(err,user)
-{
-  console.log(user);
-  console.log(user.password);
-  if (err) { return done(err); }
-  if (!user) {
-    return done(null, false, { message: 'Incorrect username.' });
-  }
-
-  else if(user.password===password)
-  {
-    console.log(user);
-    //console.log("Login Successful")
-    //req.session.email=newusers1.email_address;
-    return done(null, user);
-
-    //console.log(req.session.email);
-  }
-  else{
-    return done(null,false)
-  }
-
-})
-
-}));
-
-
-
-//passport.use('login',new LocalStrategy(
-  //function(username, password, done) {
-    //console.log("In PAssport Local Strategy method");
-    //users.findOne({ username: req.body.email_address }, function (err, user) {
-     // if (err) { return done(err); }
-      //if (!user) {
-        //return done(null, false, { message: 'Incorrect username.' });
-      //}
-      //if (!user.validPassword(password)) {
-        //return done(null, false, { message: 'Incorrect password.' });
-      //}
-      //return done(null, user);
-    //});
-  //}
-//));
-
-
-router.post('/login1',function(req,res){
-  passport.authenticate('local', function(err, user, info) {
-    console.log("Returned user in login api");
-    console.log(user);
-    console.log(user.email_address);
-
-
-    if (err)
-    {
-      throw err;
-    }
-    if (!user)
-    {
-       res.json("Please enter correct credentials");
-   }
-    else{
-
-        req.session.email=user.email_address;
-         res.json("Login Successful");
-
-
-      //res.json();
 
     }
-
-
-})(req,res);
-}
 );
 
+router.post('/logout', function (req, res) {
 
-
-//
-router.get('/checklogin', function(req,res)
-{
-  //console.log("hi");
-  if(req.session.email)
-  {
-
-    console.log("Already Logged in");
-    res.json("Already Logged in");
-
-  }
-  else{
-    console.log("Plz Log in");
-    res.json("Please Login in");
-
-  }
-  });
-
-
-router.post('/logout', function(req,res)
-{
-
-  req.session.destroy();
-	console.log('Session destroyed');
-res.json("Logged out successfully");
+    req.session.destroy();
+    console.log('Session destroyed');
+    res.json("Logged out successfully");
 
 
 })
--- cashapp client/src/components/Account.js
+++ cashapp client/src/components/Account.js
@@ -1,98 +1,66 @@
 import React, {Component} from 'react';
-import {Link,withRouter} from 'react-router-dom';
+import {Link, withRouter} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import * as API from '../api/API';
 import PieChart from 'react-simple-pie-chart';
 
 class Account extends Component {
 
-    static propTypes = {
-        
-    };
+    static propTypes = {};
     state = {
-        history:[],
-        balance:[],
-        outgoing:[],
-        incoming:[],
-        totalbal:''
+        history: [],
+        balance: [],
+        outgoing: [],
+        incoming: [],
+        totalbal: ''
     };
-    componentWillMount(){
+
+    componentWillMount() {
         API.getBalance()
-        .then(bal=>{
-            console.log(bal);
-            for(var i=0; i<bal.length;i++)
-            {
-                this.state.balance=Number(this.state.balance)+Number(bal[i].Amount);
-                
-            }
-            for(var i=0; i<bal.length;i++)
-            {
-                //this.state.balance=Number(this.state.balance)+Number(bal[i].Amount,10);
-                if(Number(bal[i].Amount)<0)
-                {
-                    this.state.outgoing=Number(this.state.outgoing)+Number(bal[i].Amount);
-                    
+            .then(user => {
+                    console.log(user);
+                    this.setState({
+                        balance: user.balance
+                    });
+
+                    console.log(this.state.balance);
+
                 }
-                else
-                {
-                    this.state.incoming=Number(this.state.incoming)+Number(bal[i].Amount);
-                    
-                }
-                
-                
-            }
-            console.log(this.state.outgoing);
-                console.log(this.state.incoming);
-            this.setState(
-                {
-                history:bal,
-                
-                }
-            )
-            console.log(this.state.balance);
-    
-            
-           
-        }
-        );
+            );
 
     }
 
     render() {
-        const projectNode=this.state.history.map((balance,index)=>{
-            
-            return(
-                   <tr key={index+1}>
-                   <td className="text-left">{index+1}</td>
-                   <td className="text-left">{balance._id}</td>
-                   <td className="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{balance.Amount}</td>
-                   <td className="text-left">{balance.email_address}</td>
-                   
-                   
-                   </tr>
-                   
-   
-            )
-             });
+        // const projectNode = this.state.history.map((balance, index) => {
+        //
+        //     return (
+        //         <tr key={index + 1}>
+        //             <td className="text-left">{index + 1}</td>
+        //             <td className="text-left">{balance._id}</td>
+        //             <td className="text-left">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{balance.Amount}</td>
+        //             <td className="text-left">{balance.email_address}</td>
+        //
+        //
+        //         </tr>
+        //
+        //
+        //     )
+        // });
 
 
-           var totalbal=function()
-           {
-               for(var i=0;i<=this.state.balance.length;i++)
-               {
-                   this.state.totalbal=this.state.balance[i];
-                   
-               }
-               return (this.state.totalbal);
-           }
+        var totalbal = function () {
+            for (var i = 0; i <= this.state.balance.length; i++) {
+                this.state.totalbal = this.state.balance[i];
 
-                
-             
-             
+            }
+            return (this.state.totalbal);
+        };
+
+
         return (
             <div className="row justify-content-md-center">
                 <div className="col-md-7">
-                {totalbal}
+                    {totalbal}
                     <form>
                         <br/>
                         <br/>
@@ -104,75 +72,55 @@ class Account extends Component {
                             <h4>Account Balance:{this.state.balance}</h4>
                         </div>
                         <div className="form-group">
-                        
-                        <br/>
-                        <Link to="/addcard"> <button
-                                className="btn btn-primary"
-                                type="button"
-                                  //since this is child to NewerHomePage
-                                                                                     //it cannot change state directly so using
-                                                                                     //props to pass it to main class(NeweHomePage)
-                                                                                     //onclick will pass the current state to main class
-                                
-                                >  
-                                Add Money
-                            </button>
+
+                            <br/>
+                            <Link to="/addcard">
+                                <button
+                                    className="btn btn-primary"
+                                    type="button"
+                                    //since this is child to NewerHomePage
+                                    //it cannot change state directly so using
+                                    //props to pass it to main class(NeweHomePage)
+                                    //onclick will pass the current state to main class
+
+                                >
+                                    Add Money
+                                </button>
                             </Link>
                             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                            <Link to="/withdraw"> <button
-                                className="btn btn-primary"
-                                type="button"
-                                  //since this is child to NewerHomePage
-                                                                                     //it cannot change state directly so using
-                                                                                     //props to pass it to main class(NeweHomePage)
-                                                                                     //onclick will pass the current state to main class
-                                
-                                >  
-                                Withdraw Money
-                            </button>
+                            <Link to="/withdraw">
+                                <button
+                                    className="btn btn-primary"
+                                    type="button"
+                                    //since this is child to NewerHomePage
+                                    //it cannot change state directly so using
+                                    //props to pass it to main class(NeweHomePage)
+                                    //onclick will pass the current state to main class
+
+                                >
+                                    Withdraw Money
+                                </button>
                             </Link>
 
                             &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
-                            <Link to="/payment"> <button
-                                className="btn btn-primary"
-                                type="button"
-                                  //since this is child to NewerHomePage
-                                                                                     //it cannot change state directly so using
-                                                                                     //props to pass it to main class(NeweHomePage)
-                                                                                     //onclick will pass the current state to main class
-                                
-                                >  
-                                Send Money
-                            </button>
+                            <Link to="/payment">
+                                <button
+                                    className="btn btn-primary"
+                                    type="button"
+                                    //since this is child to NewerHomePage
+                                    //it cannot change state directly so using
+                                    //props to pass it to main class(NeweHomePage)
+                                    //onclick will pass the current state to main class
+
+                                >
+                                    Send Money
+                                </button>
                             </Link>
-                            </div>
+                        </div>
 
-                        </form>
-                        <div>
-                            <br/>
-                            <br/>
-                            <br/>
-                        <h3>Transaction History</h3>
-                            <table className="table table-sm table-hover">
-<thead>
-    <tr>
-        <th>#</th>
-        <th>Transaction ID</th>
-        <th>Transaction Amount</th>
-        <th>Added /Deducted By</th>
-        
-</tr>
-    </thead>
-    <tbody>
-        {projectNode}
-        
-        </tbody>
-
-        
-    </table>
-                            </div>
+                    </form>
                 </div>
-                
+
             </div>
         );
     }
